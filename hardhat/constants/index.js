a8fb11621bc353d8d0d319af3321f9bb80c0b1c6
mitsue-eth <mitsue.eth@gmail.com>
2022-07-18 17:48:17 +0200

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,16 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0x0cc6AE1f2DbdD18C4591d6D2e928ae5cF6287e8d';
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+	'0x0fe19cfc3e8789d1551121f28f719f84341c8b42';
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://crypto-devs-phi.vercel.app/api/';
+
+const CryptoDevTokenContractAddress =
+	'0x63561c212D5410918524dc8Bb27BC0f945F3292c';
+
+module.exports = {
+	WHITELIST_CONTRACT_ADDRESS,
+	CRYPTO_DEVS_NFT_CONTRACT_ADDRESS,
+	METADATA_URL,
+	CryptoDevTokenContractAddress,
+};
